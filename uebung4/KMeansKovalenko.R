8ad1e7ac97e49f5814f4c2c0f1b6ae4899a9ff0a
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2019-05-12 09:04:22 +0200

    update uebung 4

--- uebung4/KMeansKovalenko.R
+++ uebung4/KMeansKovalenko.R
@@ -1,4 +1,5 @@
 
+
 #using R library
 kmeanArray = kmeans(mtcars, 5)
 datafr = data.frame("means" = kmeanArray[1][1])
@@ -7,29 +8,36 @@ datafrClusters = data.frame("centers" = kmeanArray[2])
 library(plotly)
 library(MASS)
 
-parcoord(datafrClusters, col=rainbow(sample.int(5,5)))
+dir.create(file.path(getwd(), 'kovalenkoRodionUebung4'))
 
+
+colnames(datafrClusters) <- colnames(mtcars)
+#save file
+pdf('kovalenkoRodionUebung4/parellelCoorPlot.pdf')
+
+parcoord(datafrClusters, col = sample.int(5, 5))
+dev.off()
 
 # or implement kmeans
-# 
+#
 # #generate 5 datapoints with dimenstion [1, 11]
 # centerData = c()
-# 
+#
 # for (i in 1:6) {
 #   rnd = sample.int(20, 11)
 #   centerData = c(centerData, rnd)
 # }
-# 
+#
 # centroids = array(centerData, dim = c(5, 11))
 # centroids
-# 
-# 
+#
+#
 # #cars array
 # cars = mtcars
 # num_cols = 11
-# 
+#
 # for (i in 1:nrow(cars)) {
 #   for (j in 1:nrow(centroids)) {
-#     
+#
 #   }
 # }
--- uebung4/Uebung4Kovalenko.R
+++ uebung4/Uebung4Kovalenko.R
@@ -1,4 +1,7 @@
 
+
+
+
 #install packages if needed => comment it out and execute
 # install.packages("ggplot2", dependencies = TRUE)
 # install.packages("ggExtra",dependencies=TRUE)
@@ -6,25 +9,28 @@
 library(ggplot2)
 library(ggExtra)
 
-dir.create(file.path(getwd(), 'kovalenkoRodionUebung4'));
+dir.create(file.path(getwd(), 'kovalenkoRodionUebung4'))
+
 
 #===================================================
-# a) 
+# a)
 
 species = unique(iris['Species'])
 #transpose array to dimension [1][3]
 species = array(t(species))
 #create empty matrix for filling in the variances
-variances = matrix(nrow=0,ncol=2)
+variances = matrix(nrow = 0, ncol = 2)
 #column names for plotting
-colspecies =  species;
-colvariances = c();
+colspecies =  species
+
+colvariances = c()
+
 
 #fill in the for variances in variances
 
-for(i in 1:length(species)) {
+for (i in 1:length(species)) {
+  kindSpecies = subset(iris, iris[, 'Species'] == species[i])
   
-  kindSpecies = subset(iris, iris[,'Species'] == species[i]);
   variance = var(kindSpecies['Petal.Length'])
   variances = rbind(variances, c(variance, species[i]))
   colvariances = c(colvariances, variance)
@@ -37,8 +43,8 @@ data = data.frame("species" = colspecies, "variance" = colvariances)
 #display variances
 variances
 
-#plot the data to show variance 
-ggplot(data, aes(x=species, y=variance)) + geom_point()
+#plot the data to show variance
+ggplot(data, aes(x = species, y = variance)) + geom_point()
 
 #save file
 ggsave("kovalenkoRodionUebung4/plotVariance.png")
@@ -46,7 +52,7 @@ ggsave("kovalenkoRodionUebung4/plotVariance.png")
 # b)
 
 #plot data to show outliers
-ggplot(iris, aes(x=Sepal.Width, y=Sepal.Length,color=Species)) + geom_point()
+ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) + geom_point()
 
 #save file
 ggsave("kovalenkoRodionUebung4/plotOutliers.png")
@@ -59,37 +65,54 @@ pwidth = iris['Petal.Width']
 slength = iris['Sepal.Length']
 swidth = iris['Sepal.Width']
 
-data = data.frame("species" = iris['Species'],"plength" = plength, "pwidth" = pwidth, "slength" = slength, "swidth" = swidth)
+data = data.frame(
+  "species" = iris['Species'],
+  "plength" = plength,
+  "pwidth" = pwidth,
+  "slength" = slength,
+  "swidth" = swidth
+)
 
-#plot the correlation between data 
-plot(data, 
-     col = ifelse(species == 'versicolor','red', 
-                  ifelse(species == 'virginica','green', 
-                         ifelse(species =='setosa', 'blue', 'black'))))
+#plot the correlation between data
+plot(data,
+     main = 'Correlation',
+     
+     
+     col = ifelse(
+       species == 'versicolor',
+       'red',
+       ifelse(
+         species == 'virginica',
+         'green',
+         ifelse(species == 'setosa', 'blue', 'black')
+       )
+     ))
+
 
 
 #save file
-pdf('kovalenkoRodionUebung4/correlationAll.pdf');
+pdf('kovalenkoRodionUebung4/correlationAll.pdf')
 
-
-
-plot(data, 
-     col = ifelse(species == 'versicolor','red', 
-                  ifelse(species == 'virginica','green', 
-                         ifelse(species =='setosa', 'blue', 'black'))))
+plot(data,
+     main = 'Correlation',
+     col = ifelse(
+       species == 'versicolor',
+       'red',
+       ifelse(
+         species == 'virginica',
+         'green',
+         ifelse(species == 'setosa', 'blue', 'black')
+       )
+     ))
 
 dev.off()
 
 
 # show correlation indexes
-data = data.frame("plength" = plength, "pwidth" = pwidth, "slength" = slength, "swidth" = swidth)
-cor(data, method='spearman')
-
-
-
-
-
-
-
-
-
+data = data.frame(
+  "plength" = plength,
+  "pwidth" = pwidth,
+  "slength" = slength,
+  "swidth" = swidth
+)
+cor(data, method = 'spearman')
